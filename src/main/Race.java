e65cf68fa6eb6c2a2dad5a0f7de0d1089b9df904
Himanshu Gaur <himanshu.gaur_cs19@gla.ac.in>
2020-10-29 22:26:59 +0530

    import javax

--- src/main/Race.java
+++ src/main/Race.java
@@ -3,6 +3,8 @@ package main;
 import thread.rabbit.Rabbit;
 import thread.tortoise.Tortoise;
 
+import javax.swing.*;
+
 public class Race {
     public static void main(String[] args) {
         Rabbit rabbit = new Rabbit();

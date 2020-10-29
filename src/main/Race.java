b43311b74776015f364656fb26e3846fcbf6dc3f
Himanshu Gaur <himanshu.gaur_cs19@gla.ac.in>
2020-10-29 22:28:20 +0530

    create and start tortoise object

--- src/main/Race.java
+++ src/main/Race.java
@@ -9,5 +9,7 @@ public class Race {
     public static void main(String[] args) {
         Rabbit rabbit = new Rabbit();
         rabbit.getRabbit().start();
+		Tortoise tortoise = new Tortoise;
+		tortoise.getTortoise().start();
     }
 }

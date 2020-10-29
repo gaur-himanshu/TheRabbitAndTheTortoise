c58c1e5dc94fd9f05f70959101123971b168c581
Himanshu Gaur <himanshu.gaur_cs19@gla.ac.in>
2020-10-29 22:24:17 +0530

    override run method in tortoisr

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -18,4 +18,31 @@ public class Tortoise extends Thread {
 	public Thread getTortoise() {
 		return tortoise;
 	}
+	
+	@Override
+    public void run() {
+        for (int index = 0; index < MILESTONES; index++) {
+            switch (index) {
+                case 0:
+                    System.out.println("The Tortoise has started from the start line!");
+                    break;
+                case 1:
+                    System.out.println("The Tortoise has reached the Cool River!");
+                    break;
+                case 2:
+                    System.out.println("The Tortoise has reached the Mountain Hill!");
+                    break;
+                case 3:
+                    System.out.println("The Tortoise has reached the Big Oak Tree!");
+                    break;
+                case 4:
+                    System.out.println("The Tortoise has reached the finish line!");
+            }
+            try {
+                Thread.sleep(3000L);
+				raceTime += 3;
+            } catch (InterruptedException e) {
+                System.out.println("The Tortoise has lost the path!");
+            }
+        }
 }

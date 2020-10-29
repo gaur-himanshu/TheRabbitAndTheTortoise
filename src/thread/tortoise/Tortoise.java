fb9f56c49ef7c88f8639a576dc78336c0faf5f7d
Himanshu Gaur <himanshu.gaur_cs19@gla.ac.in>
2020-10-29 22:21:05 +0530

    declare or initialize required variable

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -1,4 +1,9 @@
 package thread.tortoise;
 
 public class Tortoise extends Thread {
+	
+	public static final int MILESTONE = 5;
+
+	public final Thread tortoise;
+	private float raceTimeTortoise = 0f;
 }
